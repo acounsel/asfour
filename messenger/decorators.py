0e067baa0b99049776dd5b5a73a26d7ea864c822
Samer <sarrabi@gmail.com>
2020-03-28 13:13:37 -0700

    update validator decorator

--- messenger/decorators.py
+++ messenger/decorators.py
@@ -3,14 +3,18 @@ from functools import wraps
 from twilio import twiml
 from twilio.request_validator import RequestValidator
 
+from .models import Organization
+
 import os
 
 def validate_twilio_request(f):
-    """Validates that incoming requests genuinely originated from Twilio"""
+    """Validates incoming requests originated from Twilio"""
     @wraps(f)
     def decorated_function(request, *args, **kwargs):
         # Create an instance of the RequestValidator class
-        validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
+        org = Organization.objects.get(
+            id=self.kwargs.get('pk'))
+        validator = RequestValidator(org.twilio_secret)
 
         # Validate the request using its URL, POST data,
         # and X-TWILIO-SIGNATURE header
@@ -19,8 +23,8 @@ def validate_twilio_request(f):
             request.POST,
             request.META.get('HTTP_X_TWILIO_SIGNATURE', ''))
 
-        # Continue processing the request if it's valid, return a 403 error if
-        # it's not
+        # Continue processing the request if it's valid, 
+        # return a 403 error if it's not
         if request_valid:
             return f(request, *args, **kwargs)
         else:
